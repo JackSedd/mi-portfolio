9c4e4cde865891134e1fb8609bd6af75a4421bb0
Santi <scgtube23@gmail.com>
2025-10-08 18:43:37 +0200

    Prepare project for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // habilita next export
+  images: {
+    unoptimized: true, // necesario para <Image /> de Next.js en export estático
+  },
+  trailingSlash: true, // para que las rutas generen /index.html y funcionen en GitHub Pages
 };
 
 export default nextConfig;
